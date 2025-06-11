02ccd9cd727d4c70e1aad5e4562eaf673d0ecc02
mamesalo <117835018+mamesalo@users.noreply.github.com>
2025-06-11 17:21:15 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,8 @@ app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+app.listen(5020, () => {
+  console.log("Server is running on port 5020!");
 });
 
 app.use("/api", routes);
